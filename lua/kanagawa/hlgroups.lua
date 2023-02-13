a9c0326f34c645dddea11949e8410fb8ae68d84a
leo-moooney <118218097+leo-moooney@users.noreply.github.com>
2023-02-14 11:34:52 +1300

    Update hlgroups.lua

--- lua/kanagawa/hlgroups.lua
+++ lua/kanagawa/hlgroups.lua
@@ -326,7 +326,7 @@ function M.setup(colors, config)
         LeapBackdrop = { fg = colors.fg_comment},
         LeapLabelPrimary = { bold = true, fg = colors.fg_dark},
         LeapLabelSecondary = { bold = true, fg = colors.fg},
-        LeapMatch = {underline=true, bg = colors.bg, bold = true, fg = colors.fg_dark},
+        LeapMatch = { bg = colors.bg_search, bold = true, fg = colors.fg_dark},
 
 
         -- NvimTree                       = {},
